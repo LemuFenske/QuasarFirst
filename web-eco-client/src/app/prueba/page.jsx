41d35951d521792c45fb4eb892993ffcddf77fb7
Lemuel Fenske <lemueljosias.03@gmail.com>
2023-08-31 10:03:59 -0300

    prueba1

--- web-eco-client/src/app/prueba/page.jsx
+++ web-eco-client/src/app/prueba/page.jsx
@@ -9,7 +9,7 @@ export default function Plans() {
         width={200}
         height="315"
         src='https://www.youtube.com/embed/eZsUA23VXfw?si=8NJnjSp-2v2zsIWx'
-        title={`YouTube video: ${nombre}`}
+        title={`YouTube video: Lemu`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
