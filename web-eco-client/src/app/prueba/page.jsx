ea1873c4e30fa5bd698e530ac1532b2c03cc1523
Lemuel Fenske <lemueljosias.03@gmail.com>
2023-08-31 18:40:34 -0300

    prueba3

--- web-eco-client/src/app/prueba/page.jsx
+++ web-eco-client/src/app/prueba/page.jsx
@@ -2,18 +2,21 @@
 
 export default function Plans() {
   return (
-    <div className='flex flex-col bg-red-500 mx-2 mt-20'>
+    <div className='flex flex-col bg-red-500'>
+        <div className='flex flex-col bg-blue-500 mt-20'>
+
       <h2>Lemu</h2>
       <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque molestiae at eaque quo ea</h4>
       <iframe
-        width={0}
-        height={0}
+        width="100%" 
+        height="auto"
         src='https://www.youtube.com/embed/eZsUA23VXfw?si=8NJnjSp-2v2zsIWx'
         title={`YouTube video: Lemu`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+        </div>
     </div>
   );
 }
